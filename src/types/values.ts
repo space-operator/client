80af6fee41e3220656799a85ffa7fe8ff1bd3f33
Duy Do <duydo@protonmail.com>
2024-02-20 13:57:13 +0700

    0.11

--- src/types/values.ts
+++ src/types/values.ts
@@ -18,6 +18,20 @@ export interface IValue {
   M?: Record<string, IValue>;
 }
 
+function maybePublicKey(x: any): boolean {
+  if (x == null) return false;
+  return (
+    typeof x.toBase58 === 'function' &&
+    x.toBuffer === 'function' &&
+    x.toBuffer()?.byteLength === 32
+  );
+}
+
+function maybeKeypair(x: any): boolean {
+  if (x == null) return false;
+  return maybePublicKey(x.publicKey) && x.secretKey?.byteLength === 32;
+}
+
 export class Value implements IValue {
   S?: string;
   D?: string;
@@ -46,83 +60,83 @@ export class Value implements IValue {
     return Object.values(this)[0];
   }
 
-  // public static fromJSValue(
-  //   x: any,
-  //   customConvert?: (x: any) => Value | null
-  // ): Value | null {
-  //   switch (typeof x) {
-  //     case 'number':
-  //       return new Value({ D: x.toString() });
-  //     case 'boolean':
-  //       return new Value({ B: x });
-  //     case 'string':
-  //       return new Value({ S: x });
-  //     case 'undefined':
-  //       return new Value({ N: 0 });
-  //     case 'function':
-  //       return null;
-  //     case 'bigint':
-  //       return new Value({ D: x.toString() });
-  //     case 'object':
-  //       if (x === null) {
-  //         return new Value({ N: 0 });
-  //       }
-  //       if (x.length != null) {
-  //         return new Value({
-  //           A: Array.from(x)
-  //             .map((x) => Value.fromJSValue(x, customConvert))
-  //             .filter((x) => x != null) as IValue[],
-  //         });
-  //       }
-  //       if (x instanceof PublicKey) {
-  //         return new Value({ B3: x.toBase58() });
-  //       }
-  //       if (x instanceof Keypair) {
-  //         return new Value({
-  //           B6: bs58.encode([...x.secretKey, ...x.publicKey.encode()]),
-  //         });
-  //       }
-  //       if (x.byteLength != null) {
-  //         switch (x.byteLength) {
-  //           case 32:
-  //             return new Value({
-  //               B3: bs58.encode(x),
-  //             });
-  //           case 64:
-  //             return new Value({
-  //               B6: bs58.encode(x),
-  //             });
-  //           default:
-  //             return new Value({
-  //               BY: x.toString('base64'),
-  //             });
-  //         }
-  //       }
-  //       if (customConvert != null) {
-  //         const result = customConvert(x);
-  //         if (result != null) {
-  //           return result;
-  //         }
-  //       }
-  //       return new Value({
-  //         M: Object.fromEntries(
-  //           Object.entries(x)
-  //             .map(([k, v]) => [k, Value.fromJSValue(v, customConvert)])
-  //             .filter(([_k, v]) => v != null)
-  //         ),
-  //       });
-  //     default:
-  //       return null;
-  //   }
-  // }
+  public static fromJSValue(
+    x: any,
+    customConvert?: (x: any) => Value | null
+  ): Value | null {
+    switch (typeof x) {
+      case 'function':
+        return null;
+      case 'number':
+        return new Value({ D: x.toString() });
+      case 'boolean':
+        return new Value({ B: x });
+      case 'string':
+        return new Value({ S: x });
+      case 'undefined':
+        return new Value({ N: 0 });
+      case 'bigint':
+        return new Value({ I1: x.toString() });
+      case 'symbol':
+        return new Value({ S: x.toString() });
+      case 'object':
+        if (x === null) {
+          return new Value({ N: 0 });
+        }
+        if (maybePublicKey(x)) {
+          return new Value({ B3: x.toBase58() });
+        }
+        if (maybeKeypair(x)) {
+          return new Value({
+            B6: bs58.encode([...x.secretKey, ...x.publicKey.encode()]),
+          });
+        }
+        if (x.byteLength != null) {
+          switch (x.byteLength) {
+            case 32:
+              return new Value({
+                B3: bs58.encode(x),
+              });
+            case 64:
+              return new Value({
+                B6: bs58.encode(x),
+              });
+            default:
+              return new Value({
+                BY: x.toString('base64'),
+              });
+          }
+        }
+        if (customConvert != null) {
+          const result = customConvert(x);
+          if (result != null) {
+            return result;
+          }
+        }
+        if (Array.prototype.isPrototypeOf(x)) {
+          return new Value({
+            A: Array.from(x)
+              .map((x) => Value.fromJSValue(x, customConvert))
+              .filter((x) => x != null) as IValue[],
+          });
+        }
+        return new Value({
+          M: Object.fromEntries(
+            Object.entries(x)
+              .map(([k, v]) => [k, Value.fromJSValue(v, customConvert)])
+              .filter(([_k, v]) => v != null)
+          ),
+        });
+    }
+  }
 
   public toJSObject(): any {
     if (this.S != null) return this.S;
     if (this.D != null) return parseFloat(this.D);
     if (this.I != null) return parseFloat(this.I);
     if (this.U != null) return parseFloat(this.U);
-    if (this.I1 != null) return parseFloat(this.I1);
-    if (this.U1 != null) return parseFloat(this.U1);
+    if (this.I1 != null) return BigInt(this.I1);
+    if (this.U1 != null) return BigInt(this.U1);
     if (this.F != null) return parseFloat(this.F);
     if (this.B != null) return this.B;
     if (this.N != null) return null;
